ec018ff3039bd18b4addf83a3ae99d7e26b5793e
Danya_Vid <danilviduykov@gmail.com>
2022-08-30 09:17:20 +0500

    fix

--- src/basket/basket.module.ts
+++ src/basket/basket.module.ts
@@ -1,9 +1,13 @@
+import { Basket, BasketSchema } from './basket.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { BasketController } from './basket.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Basket.name, schema: BasketSchema }]),
+  ],
   providers: [BasketService],
   controllers: [BasketController],
 })
